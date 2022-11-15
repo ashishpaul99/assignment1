599c8514b3449691fbee0ae3bf4e0bf3d6942d3c
ashishpaul99 <ashishpaul1199@gmail.com>
2022-11-15 14:23:57 +0530

    second commit

---
+++ question2.java
@@ -0,0 +1,23 @@
+package fullstackjava;
+
+public class assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//2. Write a program to print
+//		1 1 1 1
+//		2 2 2 2
+//		3 3 3 3
+//		4 4 4 4
+//	CODE	
+     int n=5;
+     for(int i=1;i<n ;i++) {
+    	 for(int j=1;j<n ;j++) {
+    		 System.out.print(i);
+    	 }
+    	 System.out.println( );
+     }
+		
+	}
+
+}
